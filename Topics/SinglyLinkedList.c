bd6218ae142c913c0a5b1c658c59cbad35bd9552
mubashir12-a <ma2625645@gmail.com>
2025-07-21 23:42:09 +0530

    21-07-2025

---
+++ Topics/SinglyLinkedList.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main(){
+
+    struct Node *node_1 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_2 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_3 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_4 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_5 = (struct Node*)malloc(sizeof(struct Node));
+
+    node_1 -> data = 10;
+    node_1 -> next = node_2;
+
+    node_2 -> data = 20;
+    node_2 -> next = node_3;
+
+    node_3 -> data = 30;
+    node_3 -> next = node_4;
+
+    node_4 -> data = 40;
+    node_4 -> next = node_5;
+
+    node_5 -> data = 50;
+    node_5 -> next = NULL;
+
+    struct Node *head = node_1;
+
+    while(head != NULL){
+        printf("[%d | %x]\n", head -> data, head -> next);
+        head = head -> next;
+    }
+
+    return 0;
+}
