daca8964f3065977f278d0bf6c267eb33af4aa6a
mubashir12-a <ma2625645@gmail.com>
2025-07-21 23:42:44 +0530

    21-07-2025

---
+++ Topics/DoublyLinkedList.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+int main(){
+
+    struct Node *node_1 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_2 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_3 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_4 = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *node_5 = (struct Node*)malloc(sizeof(struct Node));
+
+    node_1 -> data = 10;
+    node_1 -> next = node_2;
+    node_1 -> prev = NULL;
+
+    node_2 -> data = 20;
+    node_2 -> next = node_3;
+    node_2 -> prev = node_1;
+
+    node_3 -> data = 30;
+    node_3 -> next = node_4;
+    node_3 -> prev = node_2;
+
+    node_4 -> data = 40;
+    node_4 -> next = node_5;
+    node_4 -> prev = node_3;
+
+    node_5 -> data = 50;
+    node_5 -> next = NULL;
+    node_5 -> prev = node_4;
+
+
+    // Forward traversal:
+
+    struct Node *head = node_1;
+
+    printf("\n------------Forward traversal---------\n");
+
+    while(head != NULL){
+        printf("[%p | %d | %p]\n", head -> prev, head -> data, head -> next);
+        head = head -> next;
+    }
+
+    // Reverse Traversal:
+
+    struct Node *head_rev = node_5;
+
+    printf("\n------------Reverse traversal---------\n");
+
+    while(head_rev != NULL){
+        printf("[%p | %d | %p]\n", head_rev -> prev, head_rev -> data, head_rev -> next);
+        head_rev = head_rev -> prev;
+    }
+
+    return 0;
+}
